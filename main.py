226fad8aa10a654f1d8f8f8302043c87b5f1fb02
Liviu Maria <liviu.maria@modaoperandi.com>
2019-08-06 19:09:28 +0300

    initial commit working on python 3.6.8

---
+++ main.py
@@ -0,0 +1,22 @@
+from __future__ import (absolute_import, division, print_function)
+import os
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from shapely.geometry import Point
+import pandas as pd
+import geopandas as gpd
+from geopandas import GeoSeries, GeoDataFrame
+world= gpd.read_file(os.path.join('countries', "ne_10m_admin_0_countries.shp"))
+romania=world[world['NAME'] == 'Romania']
+
+europe =world[world['CONTINENT'] == 'Europe']
+
+#somewhere in bucharest it's 26.05, 44,47
+print(romania['geometry'].contains(Point(26.05, 44.47)))
+
+#somwehere in sopron
+print(romania['geometry'].contains(Point(16.582035, 47.676720)))
+
+print(europe['geometry'].contains(Point(16.582035, 47.676720)))
