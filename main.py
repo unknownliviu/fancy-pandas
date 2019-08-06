3e472507d037f0e7da6029d1d0d5fd6a29a99d73
Liviu Maria <liviu.maria@modaoperandi.com>
2019-08-06 19:20:58 +0300

    added boolean check to see if point is in any boolean country

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from shapely.geometry import Point
 import pandas as pd
 import geopandas as gpd
 from geopandas import GeoSeries, GeoDataFrame
-world= gpd.read_file(os.path.join('countries', "ne_10m_admin_0_countries.shp"))
-romania=world[world['NAME'] == 'Romania']
 
-europe =world[world['CONTINENT'] == 'Europe']
+world = gpd.read_file(os.path.join('countries', "ne_10m_admin_0_countries.shp"))
+
+romania = world[world['NAME'] == 'Romania']
+
+europe = world[world['CONTINENT'] == 'Europe']
 
 #somewhere in bucharest it's 26.05, 44,47
 print(romania['geometry'].contains(Point(26.05, 44.47)))
@@ -19,4 +21,5 @@ print(romania['geometry'].contains(Point(26.05, 44.47)))
 #somwehere in sopron
 print(romania['geometry'].contains(Point(16.582035, 47.676720)))
 
-print(europe['geometry'].contains(Point(16.582035, 47.676720)))
+# Boolean telling if point is in any european country
+print(True in(europe['geometry'].contains(Point(16.582035, 47.676720)).values))
